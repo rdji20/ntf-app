7ebc63a80b9575d539540c9b40bd187f88c242ec
Roberto <roberto.infante03@gmail.com>
2023-01-10 17:41:27 -0600

    Create log in

--- App.js
+++ App.js
@@ -1,12 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, TextInput, StyleSheet, SafeAreaView } from 'react-native';
+import { initializeApp } from 'firebase/app';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNr3BF-AxdB5HcUIZ9xj2yxcBYHo1kwd0",
+  authDomain: "ntf-app-36f1e.firebaseapp.com",
+  projectId: "ntf-app-36f1e",
+  storageBucket: "ntf-app-36f1e.appspot.com",
+  messagingSenderId: "608923799855",
+  appId: "1:608923799855:web:740b0906861387e90811da",
+  measurementId: "G-9HWLSYE15H"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
 
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const [isAuthenticated, setAuthenticated] = useState(false);
+
+  const handleSignUp = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+  };
+
+  const handleSignIn = () => {
+    signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+  };
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+    <TextInput
+      placeholder="Email"
+      value={email}
+      onChangeText={setEmail}
+    />
+    <TextInput
+      placeholder="Password"
+      value={password}
+      onChangeText={setPassword}
+      secureTextEntry
+    />
+    <Button title="Sign Up" onPress={handleSignUp} />
+    <Button title="Sign In" onPress={handleSignIn} />
+    {error && <Text>{error}</Text>}
+  </SafeAreaView>
   );
 }
 
